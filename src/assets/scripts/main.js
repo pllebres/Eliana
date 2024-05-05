e3c3fe9f77686d4b60a257bd34277169bad4c8b6
pilar llebres ferriol <pllebres@uoc.edu>
2024-05-05 22:01:22 +0200

    Primer commit

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,22 @@
+/**
+ * Import dependencies from node_modules
+ * see commented examples below
+ */
+
+// import 'some-node-module';
+// import SomeModule from 'some-node-module';
+
+/**
+ * Write any other JavaScript below
+ */
+
+
+
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+import Alert from 'bootstrap/js/dist/alert'
+
+// or, specify which plugins you need:
+import { Tooltip, Toast, Popover } from 'bootstrap'
